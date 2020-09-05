07d15bd5514fb246e98c4b9b46a46476c6c5aca5
Ajinkya G <ajinkyacodes@gmail.com>
2020-09-05 21:48:29 +0530

    Created PostController and model Post with posts table migration. Added default string length as 191 in AppServiceProvider to avoid any errors.

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/','PagesController@index');
 Route::get('/about','PagesController@about');
 Route::get('/services','PagesController@services');
+
+Route::resource('posts', 'PostController');
