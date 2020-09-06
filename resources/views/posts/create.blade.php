61c743bf9248d94ee27c6240b7243a1f0cba1b4c
Ajinkya G <ajinkyacodes@gmail.com>
2020-09-06 07:47:23 +0530

    Created form group cover_image for file upload

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1 class="mt-3 mb-4">Create Post</h1>
-    {!! Form::open(['action' => 'PostController@store', 'method' => 'POST']) !!}
+    {!! Form::open(['action' => 'PostController@store', 'method' => 'POST', 'enctype' => 'multipart/data']) !!}
         <div class="form-group">
             {{Form::label('title', 'Title')}}
             {{Form::text('title','',['class'=>'form-control', 'placeholder' => 'Title'])}}
@@ -10,6 +10,9 @@
         <div class="form-group">
             {{Form::label('body', 'Body')}}
             {{Form::textarea('body','',['class'=>'ckeditor form-control', 'placeholder' => 'Body Text'])}}
+        </div>
+        <div class="form-group">
+            {{ Form::file('cover_image') }}
         </div>
         {{ Form::submit('Submit', ['class' => 'btn btn-primary']) }}
     {!! Form::close() !!}
