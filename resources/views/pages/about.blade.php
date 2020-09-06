9bd05329ce9e9d9ffe78992829fb69c52dc47e7d
Ajinkya G <ajinkyacodes@gmail.com>
2020-09-06 09:08:14 +0530

    minor margin spacing adjusted through bootstrap classes.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     {{-- <h1>{{$title}}</h1> --}}
     {{-- We can use {{Blade_syntax}} OR Regular PHP code inside PHP syntax --}}
-    <h1><?php echo $title; ?></h1>
+    <h1 class="mb-3"><?php echo $title; ?></h1>
     <p>This is about page.</p>
 @endsection
