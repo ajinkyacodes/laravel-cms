851213618efc482973d2d37940e3a17980a64a4f
Ajinkya G <ajinkyacodes@gmail.com>
2020-09-06 07:10:43 +0530

    Created Model Relationships OneToMany for UserToPosts. Created edit and delete buttons for blog post table rows.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,14 +9,36 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
                     <a href="/posts/create" class="btn btn-primary mb-4">Create Post</a>
                     <h3>Your Blog Posts</h3>
-                    {{-- {{ __('You are logged in!') }} --}}
-                </div>
+                    @if (count($posts)>0)
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Title</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        @foreach ($posts as $post)
+                        <tr>
+                        <td>{{$post->title}}</td>
+                            <td><a href="/posts/{{$post->id}}/edit" class="btn btn-secondary">Edit</a></td>
+                            <td>
+                                {!! Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
+                                    {{ Form::hidden('_method', 'DELETE') }}
+                                    {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+                                {!! Form::close() !!}
+                            </td>
+                        </tr>
+                        @endforeach
+                    </table>
+                    @else
+                    <p>You have no posts.</p>
+                    @endif                    
+                </div>                       
             </div>
         </div>
     </div>
